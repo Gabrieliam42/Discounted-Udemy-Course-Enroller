1ca410eb5324079a9df6e8970fad07d60dca4065
Gabrieliam42 <sander.michael.gabriel@gmail.com>
2024-09-10 22:00:52 +0300

    Updated base.py
    Fixed IndexError in Error handling by checking list length.

--- base.py
+++ base.py
@@ -768,7 +768,7 @@ class Udemy:
         instructors = [
             i["absolute_url"].split("/")[-2]
             for i in dma["serverSideProps"]["course"]["instructors"]["instructors_info"]
-            if i["absolute_url"]
+            if len(i["absolute_url"].split("/")) > 1  # Ensure there are enough elements
         ]
         lang = dma["serverSideProps"]["course"]["localeSimpleEnglishTitle"]
         cat = dma["serverSideProps"]["topicMenu"]["breadcrumbs"][0]["title"]
